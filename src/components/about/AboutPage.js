c6e72390313b70fa4c652895cdf2e496c89048bd
gloria <cmxgloria@gmail.com>
2021-05-21 22:11:23 +1000

    created header and aboutPage

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -3,6 +3,8 @@ import React from 'react';
 const AboutPage = () => (
   <div className="jumbotron">
     <h1>About page</h1>
+    <p>Love coding? How to improve skill? See as below...</p>
+    <p>For this app, we use react, redux and react router.</p>
   </div>
 );
 
--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Router } from 'react-router';
-import { BrowserRouter as Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './components/App';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
-  // 'app'
+  // <React.StrictMode>
+  <Router>
+    <App />
+  </Router>,
+  // </React.StrictMode>
+  document.getElementById('app')
+  // 'root' originally
 );
 
 // If you want to start measuring performance in your app, pass a function
