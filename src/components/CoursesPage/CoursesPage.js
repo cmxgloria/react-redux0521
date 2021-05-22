ba4cc063b34641771784108c1e9cf4db163bc440
gloria <cmxgloria@gmail.com>
2021-05-22 20:58:37 +1000

    added redux

--- src/components/CoursesPage/CoursesPage.js
+++ src/components/CoursesPage/CoursesPage.js
@@ -1,15 +1,39 @@
 import React from 'react';
 
 class CoursesPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      course: {
+        title: '',
+      },
+    };
+  }
+
+  handleChange(event) {
+    const course = { ...this.state.course, title: event.target.value };
+    this.setState({ course: course });
+  }
+
   render() {
     return (
       <div>
-        <ul>
-          <li>React</li>
-          <li>Redux</li>
-          <li>Javascript</li>
-          <li>Ruby</li>
-        </ul>
+        <form action="">
+          <h2>Courses</h2>
+          <h3>Add Course</h3>
+          <input
+            type="text"
+            value={this.state.course.title}
+            onChange={this.handleChange.bind(this)}
+          />
+          <input type="submit" value="Save" />
+          <ul>
+            <li>React</li>
+            <li>Redux</li>
+            <li>Javascript</li>
+            <li>Ruby</li>
+          </ul>
+        </form>
       </div>
     );
   }
