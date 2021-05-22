43026f1ee0b5de6ce0b256846a584132cf067c58
gloria <cmxgloria@gmail.com>
2021-05-22 21:11:14 +1000

    arrow func to inherit binding

--- src/components/CoursesPage/CoursesPage.js
+++ src/components/CoursesPage/CoursesPage.js
@@ -1,39 +1,44 @@
 import React from 'react';
 
 class CoursesPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      course: {
-        title: '',
-      },
-    };
-  }
+  state = {
+    course: {
+      title: '',
+    },
 
-  handleChange(event) {
+    // this.handleChange = this.handleChange.bind(this);
+  };
+
+  // arrow function inherit the binding context of their enclosing scope
+  handleChange = (event) => {
     const course = { ...this.state.course, title: event.target.value };
     this.setState({ course: course });
-  }
+  };
 
+  handleSubmit = (event) => {
+    // this prevent form from causing the page to reload, prevent browser default behavior
+    event.preventDefault();
+    alert(this.state.course.title);
+  };
   render() {
     return (
       <div>
-        <form action="">
+        <form onSubmit={this.handleSubmit}>
           <h2>Courses</h2>
           <h3>Add Course</h3>
           <input
             type="text"
             value={this.state.course.title}
-            onChange={this.handleChange.bind(this)}
+            onChange={this.handleChange}
           />
           <input type="submit" value="Save" />
-          <ul>
-            <li>React</li>
-            <li>Redux</li>
-            <li>Javascript</li>
-            <li>Ruby</li>
-          </ul>
         </form>
+        <ul>
+          <li>React</li>
+          <li>Redux</li>
+          <li>Javascript</li>
+          <li>Ruby</li>
+        </ul>
       </div>
     );
   }
