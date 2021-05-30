d1b902bedfba18cb26e505b889c4efdd5bffe3b7
gloria <cmxgloria@gmail.com>
2021-05-30 12:48:28 +1000

    added manageCoursePage

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -1,80 +1,50 @@
-// //have not finished
-// import React from 'react';
-// import { connect } from 'react-redux';
-// import * as courseActions from '../redux/actions/courseActions';
-// import PropTypes from 'prop-types';
-// import { bindActionCreators } from 'redux';
+import React from 'react';
+import { connect } from 'react-redux';
+import { loadCourses } from '../redux/actions/courseActions';
+import { loadAuthors } from '../redux/actions/authorActions';
+import PropTypes from 'prop-types';
 
-// class ManageCoursePage extends React.Component {
-//   state = {
-//     course: {
-//       title: '',
-//     },
+class ManageCoursePage extends React.Component {
+  componentDidMount() {
+    const { courses, authors, loadCourses, loadAuthors } = this.props;
+    if (courses.length === 0) {
+      loadCourses().catch((error) => {
+        alert('Loading courses failed' + error);
+      });
+    }
+    if (authors.length === 0) {
+      loadAuthors().catch((error) => {
+        alert('Loading authors failed' + error);
+      });
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h2>Manage Courses</h2>
+      </div>
+    );
+  }
+}
 
-//     // this.handleChange = this.handleChange.bind(this);
-//   };
+ManageCoursePage.propTypes = {
+  authors: PropTypes.array.isRequired,
+  courses: PropTypes.array.isRequired,
+  // createCourse: PropTypes.func.isRequired,
+  loadCourses: PropTypes.func.isRequired,
+  loadAuthors: PropTypes.func.isRequired,
+};
 
-//   // arrow function inherit the binding context of their enclosing scope
-//   handleChange = (event) => {
-//     const course = { ...this.state.course, title: event.target.value };
-//     this.setState({ course: course });
-//   };
+function mapStateToProps(state) {
+  return {
+    courses: state.authors,
+    authors: state.authors,
+  };
+}
 
-//   handleSubmit = (event) => {
-//     // this prevent form from causing the page to reload, prevent browser default behavior
-//     event.preventDefault();
-//     // this.props.createCourse(this.state.course);
-//     this.props.actions.createCourse(this.state.course);
-//     // alert(this.state.course.title);
-//   };
-//   render() {
-//     return (
-//       <div>
-//         {/* <form onSubmit={this.handleSubmit}> */}
-//         <h2>Courses</h2>
-//         {/* <h3>Add Course</h3>
-//           <input
-//             type="text"
-//             value={this.state.course.title}
-//             onChange={this.handleChange}
-//           />
-//           <input type="submit" value="Save" />
-//           <ul>
-//             <li>React</li>
-//             <li>Redux</li>
-//             <li>Javascript</li>
-//             <li>Ruby</li> */}
-//         {this.props.courses.map((course) => (
-//           <div key={course.title}>
-//             <li>{course.title}</li>
-//           </div>
-//         ))}
-//         {/* </ul>
-//         </form> */}
-//       </div>
-//     );
-//   }
-// }
+const mapDispatchToProps = {
+  loadCourses,
+  loadAuthors,
+};
 
-// ManageCoursePage.propTypes = {
-//   courses: PropTypes.array.isRequired,
-//   // createCourse: PropTypes.func.isRequired,
-//   actions: PropTypes.object.isRequired,
-// };
-
-// function mapStateToProps(state) {
-//   return {
-//     courses: state.courses,
-//   };
-// }
-
-// //Action creator must be called by dispatch, or nothing happens
-// function mapDispatchToProps(dispatch) {
-//   return {
-//     // createCourse: (course) => dispatch(courseActions.createCourse(course)),
-//     // createCourse: bindActionCreators(courseActions, dispatch),
-//     actions: bindActionCreators(courseActions, dispatch),
-//   };
-// }
-
-// export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage);
+export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage);
